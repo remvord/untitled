9bba699c17721df8df5faf32fd8b9f0eca5b39a9
remvord <rem-den@yandex.ru>
2019-04-06 22:06:06 +0300

    stepik

--- netmiko_1.py
+++ netmiko_1.py
@@ -7,6 +7,7 @@ import re
 
 start_time = time.time()
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -17,13 +18,15 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def doRouter(connection_data):
 
+def doRouter(connection_data):
     ip_address = connection_data[0]
     username = connection_data[1]
     password = connection_data[2]
 
-    ip_check = re.findall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", ip_address)
+    ip_check = re.findall(
+        "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",
+        ip_address)
     if ip_check == []:
         print(bcolors.FAIL + "Invalid IP - " + str(ip_address) + bcolors.ENDC)
         return ip_address
@@ -33,7 +36,7 @@ def doRouter(connection_data):
         'ip': ip_address.strip(),
         'username': username,
         'password': password,
-        'port': 22, }
+        'port': 2222, }
     try:
         config_ok = True
 
@@ -43,39 +46,44 @@ def doRouter(connection_data):
         cli_response = cli_response.replace("Interface: ", "")
         cli_response = cli_response.replace(", IPv4 NHRP Details", "").strip()
         if cli_response != "Tunnel1":
-            print(str(ip_address)+" - " + bcolors.WARNING + "WARNING - DMVPN not on Tunnel1.  " + cli_response+ " " + bcolors.ENDC)
-            config_ok=False
+            print(str(
+                ip_address) + " - " + bcolors.WARNING + "WARNING - DMVPN not on Tunnel1.  " + cli_response + " " + bcolors.ENDC)
+            config_ok = False
 
-        cli_response2=net_connect.send_command("sh run | i ip route 1.1.1.1 255.255.255.255")
+        cli_response2 = net_connect.send_command("sh run | i ip route 1.1.1.1 255.255.255.255")
         if cli_response2.strip() == "":
-            print(str(ip_address)+" - " + bcolors.WARNING + "WARNING - couldn't find static route to 8.8.8.8" + bcolors.ENDC)
-            config_ok=False
+            print(str(
+                ip_address) + " - " + bcolors.WARNING + "WARNING - couldn't find static route to 8.8.8.8" + bcolors.ENDC)
+            config_ok = False
 
         ip_next_hop = ""
         if cli_response2 != "":
             ip_next_hop = cli_response2.split(" ")[4]
 
         if ip_next_hop == "":
-            print(str(ip_address)+" - " + bcolors.WARNING + "WARNING - couldn't find next-hop IP address " + bcolors.ENDC)
-            config_ok=False
-
+            print(str(
+                ip_address) + " - " + bcolors.WARNING + "WARNING - couldn't find next-hop IP address " + bcolors.ENDC)
+            config_ok = False
 
         if config_ok:
-            config_commands = ['ip route 1.1.1.1 255.255.255.255 '+ip_next_hop,
-                               'ip route 2.2.2.2 255.255.255.255 '+ip_next_hop]
-         	    net_connect.send_config_set(config_commands)
+            config_commands = ['ip route 1.1.1.1 255.255.255.255 ' + ip_next_hop,
+                               'ip route 2.2.2.2 255.255.255.255 ' + ip_next_hop]
+            net_connect.send_config_set(config_commands)
             print(str(ip_address) + " - " + "Static routes added")
+
         else:
-            print(str(ip_address) + " - " + bcolors.FAIL + "Routes weren't added because config is incorrect" + bcolors.ENDC)
+            print(str(
+                ip_address) + " - " + bcolors.FAIL + "Routes weren't added because config is incorrect" + bcolors.ENDC)
             return ip_address
 
         if config_ok:
-   	                  net_connect.send_command_expect('write memory')
+            net_connect.send_command_expect('write memory')
             print(str(ip_address) + " - " + "Config saved")
 
         net_connect.disconnect()
+
     except:
-        print(str(ip_address)+" - "+bcolors.FAIL+"Cannot connect to this device."+bcolors.ENDC)
+        print(str(ip_address) + " - " + bcolors.FAIL + "Cannot connect to this device." + bcolors.ENDC)
         return ip_address
     print(str(ip_address) + " - " + bcolors.OKGREEN + "Router configured sucessfully" + bcolors.ENDC)
 
@@ -88,15 +96,15 @@ if __name__ == '__main__':
 
     try:
         f = open('ip.txt')
-        connection_data=[]
+        connection_data = []
         filelines = f.read().splitlines()
         for line in filelines:
             if line == "": continue
             if line[0] == "#": continue
             conn_data = line.split(',')
-            ipaddr=conn_data[0].strip()
-            username=global_username
-            password=global_password
+            ipaddr = conn_data[0].strip()
+            username = global_username
+            password = global_password
             if len(conn_data) > 1 and conn_data[1].strip() != "": username = conn_data[1].strip()
             if len(conn_data) > 2 and conn_data[2].strip() != "": password = conn_data[2].strip()
             connection_data.append((ipaddr, username, password))
@@ -116,10 +124,10 @@ if __name__ == '__main__':
     failed_file = open('fail.txt', 'w')
     for item in routers_with_issues:
         if item != None:
-          failed_file.write("%s\n" % item)
-          print(item)
+            failed_file.write("%s\n" % item)
+            print(item)
 
-    #Completing the script and print running time
+    # Completing the script and print running time
     print("\n")
     print("#This script has now completed#")
     print("\n")
---
+++ stepik_1.py
@@ -0,0 +1,6 @@
+x = [1, 2, 3]
+# print(id(x))
+# print(id([1, 2, 3]))
+y = x
+print(y is x)
+print(y is [1, 2, 3])
